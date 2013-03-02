dc7fa0275d9fe82952473f8284b1e09c0e10da1e
cwnga <ng_chi_wa@hotmail.com>
2013-03-02 18:34:06 +0800

    fix get user checkin

---
+++ lib/java/android/html5/src/com/example/html5/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.html5;
+
+import android.os.Bundle;
+//import android.app.Activity;
+import com.phonegap.*;
+import android.view.Menu;
+
+public class MainActivity extends DroidGap {
+
+    @Override
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index2.html");
+        //setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+    
+}
