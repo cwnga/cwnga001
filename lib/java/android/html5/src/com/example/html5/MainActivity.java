5157dce893a3e61a96054bfbd58d62b8994fdd5e
cwnga <ng_chi_wa@hotmail.com>
2013-03-03 18:32:15 +0800

    commit
    commit

--- lib/java/android/html5/src/com/example/html5/MainActivity.java
+++ lib/java/android/html5/src/com/example/html5/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends DroidGap {
     @Override
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        super.loadUrl("file:///android_asset/www/index2.html");
+        super.loadUrl("file:///android_asset/www/ws.html");
         //setContentView(R.layout.activity_main);
     }
 
